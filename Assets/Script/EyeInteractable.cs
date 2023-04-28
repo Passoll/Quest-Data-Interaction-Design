d3f0e15c7086be7b8c223e278c4143faa144a159
Zhong Peixi <zpasso2018@gmail.com>
2023-04-28 10:15:22 +0800

    Add the feature of gaze DxR
    Add the feature of gaze DxR vis

--- Assets/Script/EyeInteractable.cs
+++ Assets/Script/EyeInteractable.cs
@@ -3,20 +3,30 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(Collider))]
-[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(Rigidbody))]
 public class EyeInteractable : MonoBehaviour
 {
     public bool IsHovered { get; set; }
 
     [SerializeField] private UnityEvent<GameObject> OnObjectHover;
-    [SerializeField] private Material OnHoverActiveMat;
+    [SerializeField] private Material OnHoverActiveMat ;
     [SerializeField] private Material OnHoverInActiveMat;
 
     private MeshRenderer _meshRenderer;
-
+        
     private void Start()
     {
+        if (OnHoverActiveMat == null && OnHoverInActiveMat == null )
+        {
+            Material acMaterial = Resources.Load("Materials/acMat", typeof(Material)) as Material;
+            Material inMaterial = Resources.Load("Materials/inMat", typeof(Material)) as Material;
+            OnHoverActiveMat = acMaterial;
+            OnHoverInActiveMat = inMaterial;
+        }
+       
+        
         _meshRenderer = GetComponent<MeshRenderer>();
+        
     }
     private void Update()
     {
